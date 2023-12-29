429752880bac24f369b1244c280da68809f5dc84
nurilhijas <nurilhijas@gmail.com>
2023-12-29 22:36:09 +0530

    All Doc sent it to git

---
+++ ServerShutdown2/app/src/main/java/com/example/servershutdown/shutdown.java
@@ -0,0 +1,16 @@
+package com.example.servershutdown;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class shutdown extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      }
+
+}
